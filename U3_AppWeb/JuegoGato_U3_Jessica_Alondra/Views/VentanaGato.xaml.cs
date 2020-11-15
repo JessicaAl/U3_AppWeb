dbb9bbfa3252ffb6d6edddcefcbee0190f60885c
Alondra Delgadillo <171G0576@rcarbonifera.tecnm.mx>
2020-11-15 11:30:43 -0600

    Juego del gato

---
+++ U3_AppWeb/JuegoGato_U3_Jessica_Alondra/Views/VentanaGato.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JuegoGato_U3_Jessica_Alondra.Views
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaGato.xaml
+    /// </summary>
+    public partial class VentanaGato : Window
+    {
+        public VentanaGato()
+        {
+            InitializeComponent();
+        }
+    }
+}
